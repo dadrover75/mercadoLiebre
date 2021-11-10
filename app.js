35b4256787ce6eacfcff8e7b2421655003224044
John DoeDiana <dadrover75@hotmail.com>
2021-11-10 16:46:17 -0300

    cambios clase ml paralela (home1 y style1)

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3030 
+const port = 3030
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/home.html'))
-  })
+    res.sendFile(path.resolve(__dirname + '/views/home.html'))
+})
 
 app.listen(3030, () => console.log('Escuchando en el puerto'))
