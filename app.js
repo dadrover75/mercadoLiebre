f17765857d5e401ff0bf124927690de1c2365e60
John DoeDiana <dadrover75@hotmail.com>
2021-11-15 19:58:46 -0300

    form register y login

--- app.js
+++ app.js
@@ -7,5 +7,11 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname + '/views/home.html'))
 })
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/login.html"));
+});
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/register.html"));
+});
 
 app.listen(3030, () => console.log('Escuchando en el puerto'))
